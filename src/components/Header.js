eaa835799b5d35dc64f7f3bdbe9521a0b0d3b156
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-03-24 16:36:28 -0400

    another commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,7 @@
 import PropTypes from 'prop-types'
 import Button from './Button'
 const Header = ({ title }) => {
-  const onClick = () => {
-    console.log("Bulgary")
-    alert("click motherfucker")
+  const onClick = () => {    
   }
   return (    
     <header className="header">
