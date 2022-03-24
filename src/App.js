2f828b788a213244d0f128f45fd6ccb759558aa6
Ernesto Figueredo <ernesto@jewelersconnect.com>
2022-03-24 01:05:45 -0400

    another coomit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import Header from "./components/Header"
+import Tasks from './components/Tasks'
+import { useState } from "react"
 function App() {
+    const [tasks, setTasks] = useState([
+      { 
+        id:1,
+        text: "Go to Gym",
+        day:"March 24 at 8am",
+        reminder:true
+      },
+      {
+        id:2,
+        text: "Go to Cristo",
+        day:"March 24 at 11am",
+        reminder:true
+      },
+      {
+        id:3,
+        text: "Work",
+        day:"March 24 at 12pm",
+        reminder:false
+      }
+    ])
     return (
-        <div className = "App">
-          <h1>Hello from React</h1>
+        <div className = "container">
+          <Header/>
+          <Tasks tasks = {tasks}/>
         </div>
     );
 }
